62000d59e7d6e1dc082e11e011c8bd3e5a3879bd
Khalil <khalilnasri92@gmail.com>
2021-11-30 23:52:42 +0100

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 import './style.css';
 
 const tasks = [
-    {
-        description: 'Call the dentist at 9am',
-        completed: false,
-        index: 1,
-    },
-    {
-        description: 'Go to gym at 5pm',
-        completed: false,
-        index: 2,
-    },
-    {
-        description: 'Reply to emails',
-        completed: false,
-        index: 3,
-    },
+  {
+    description: 'Call the dentist at 9am',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Go to gym at 5pm',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Reply to emails',
+    completed: false,
+    index: 3,
+  },
 ];
 
 const todos = document.getElementById('todo-container');
 
 const displayTodo = () => {
   let htmlCode = '';
-  tasks.forEach((item) => {  
+  tasks.forEach((item) => {
     htmlCode += `
     <div class='todo-item'>
       <div class='left-items'>
@@ -32,10 +32,10 @@ const displayTodo = () => {
       <button class='remove-btn'>del</button>
     </div>
     `;
-  })
+  });
   todos.innerHTML = htmlCode;
-}
+};
 
 window.addEventListener('load', () => {
   displayTodo();
-})
+});
